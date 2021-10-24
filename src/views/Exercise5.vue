e5657c1554c0644f286c96c402f40463f77fd042
Luemmy <silence000@foxmail.com>
2021-10-24 19:49:21 +0800

    Ex5

---
+++ src/views/Exercise5.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="exercise5">
+    <img v-for="(item, index) in fileList" :key="index" :src="item" height="50" width="50" alt=""/>
+    <input type="file" ref="inputRef" @change="uploadPhoto()"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Exercise5', // 组件名称
+  components: {},
+  props: {}, // 组件参数
+  model: { // 用于 props 绑定父级 v-model
+  },
+  filters: { // 过滤器
+  },
+  computed: { // 计算属性
+  },
+  watch: { // 监听属性
+  },
+  data () {
+    return {
+      /*
+      * 页面状态
+      * */
+      fileList: []
+      /*
+      * 页面数据
+      * */
+    }
+  },
+  methods: {
+    uploadPhoto () {
+      const photo = this.$refs.inputRef.files[0]
+      this.fileList.push(photo)
+    }
+  },
+  beforeCreate () { // 播放加载动画
+  },
+  created () { // 结束加载动画, 发起异步请求
+  },
+  mounted () { // DOM构建完成, 即将显示页面
+  },
+  updated () { // view重新渲染, 数据更新
+  },
+  beforeDestroy () { // 组件实例销毁之前
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,13 @@
     <el-button @click="switchComp('Exercise2')" size="small">For Exercise</el-button>
     <el-button @click="switchComp('Exercise3')" size="small">Debounce</el-button>
     <el-button @click="switchComp('Exercise4')" size="small">Exercise4</el-button>
+    <el-button @click="switchComp('Exercise5')" size="small">Photos</el-button>
     <hr>
     <Exercise1 v-if="showComp==='Exercise1'"></Exercise1>
     <Exercise2 v-if="showComp==='Exercise2'"></Exercise2>
     <Exercise3 v-if="showComp==='Exercise3'"></Exercise3>
     <Exercise4 v-if="showComp==='Exercise4'"></Exercise4>
+    <Exercise5 v-if="showComp==='Exercise5'"></Exercise5>
   </div>
 </template>
 
@@ -18,6 +20,7 @@ import Exercise1 from '@/views/Exercise1'
 import Exercise2 from '@/views/Exercise2'
 import Exercise3 from '@/views/Exercise3'
 import Exercise4 from '@/views/Exercise4'
+import Exercise5 from '@/views/Exercise5'
 
 export default {
   name: 'Home',
@@ -25,7 +28,8 @@ export default {
     Exercise1,
     Exercise2,
     Exercise3,
-    Exercise4
+    Exercise4,
+    Exercise5
   },
   data () {
     return {
